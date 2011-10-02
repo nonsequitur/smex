8a280f5b2d510cf1f8964819357f7c21b12f023c
Cornelius Mika <cornelius.mika@gmail.com>
2011-10-02 11:37:03 +0200

    Revert "Require 'cl at compile time."
    This reverts commit 2e861743252a9f8264526569e8ca1f329229aa6f.

--- smex.el
+++ smex.el
@@ -26,7 +26,7 @@
 
 (require 'ido)
 ;; Provides `union' and `dolist'.
-(eval-when-compile (require 'cl))
+(require 'cl)
 
 (defgroup smex nil
   "M-x interface with Ido-style fuzzy matching and ranking heuristics."
