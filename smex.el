298e9f8028017af777ddf3de12665aa2c7adebc7
Daniel Hackney <dan@haxney.org>
2012-08-11 12:35:27 +0200

    Simplify `smex-major-mode-commands' use of `mapcar'
    Simply passing a symbol to `mapcar' makes it call that symbol for each element
    in the list.

--- smex.el
+++ smex.el
@@ -118,7 +118,7 @@ Set this to nil to disable fuzzy matching."
   (let ((commands (delete-dups (append (extract-commands-from-keymap (current-local-map))
                                        (extract-commands-from-features major-mode)))))
     (setq commands (smex-sort-according-to-cache commands))
-    (setq commands (mapcar (lambda (command) (symbol-name command)) commands))
+    (setq commands (mapcar #'symbol-name commands))
     (smex-read-and-run commands)))
 
 (defun smex-completing-read (choices initial-input)
