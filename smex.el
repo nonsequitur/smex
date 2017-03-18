3b7781ac3629909755973b3d27c47f0061830a74
Ryan C. Thompson <rct@thompsonclan.org>
2017-03-18 09:42:54 -0700

    Remove no-longer-necessary fix from 187a9c5
    ido-ubiquitous no longer has any effect on completions that are not
    initiated through its own functions, so this fix is no longer
    necessary.

--- smex.el
+++ smex.el
@@ -138,7 +138,7 @@ Set this to nil to disable fuzzy matching."
         (ido-max-prospects 10)
         (minibuffer-completion-table choices))
     (ido-completing-read+ (smex-prompt-with-prefix-arg) choices nil t
-                          initial-input 'extended-command-history (car choices))))
+                          initial-input 'extended-command-history)))
 
 (defun smex-prompt-with-prefix-arg ()
   (if (not current-prefix-arg)
